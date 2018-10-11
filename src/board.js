b0ee401893f7145c2fb79231aa656980149be5d9
Evan Powell <evan.powell@gmail.com>
2018-10-10 17:00:00 -0700

    add box props

--- src/board.js
+++ src/board.js
@@ -4,32 +4,35 @@ import App from './App.js'
 import Box from './box.js'
 
 class Board extends Component {
+  constructor(props){
+    super(props)
+
+    this.state={
+        index: ['','','','','','','','',''],
+        isClicked: false,
+    }
+  }
   render() {
+
     return (
-
           <div id="ourBoard">
-
-              <Box />
-              <Box />
-              <Box />
-
-
-
-              <Box />
-              <Box />
-              <Box />
-
-
-
-              <Box />
-              <Box />
-              <Box />
-
-
+              <Box index={0} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
+              <Box index={1} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
+              <Box index={2} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
+              <Box index={3} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
+              <Box index={4} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
+              <Box index={5} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
+              <Box index={6} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
+              <Box index={7} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
+              <Box index={8} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
           </div>
-
-
     );
+  }
+  printOut = () => {
+
+  }
+  handleClick = () => {
+
   }
 }
 
