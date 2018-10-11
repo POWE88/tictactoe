05bdbe6a3e5e2b7f674431fc26957897ab0a6fcd
Evan Powell <evan.powell@gmail.com>
2018-10-11 12:04:44 -0700

    made click functionality work

--- src/board.js
+++ src/board.js
@@ -9,29 +9,39 @@ class Board extends Component {
 
     this.state={
         index: ['','','','','','','','',''],
-        isClicked: false,
+        player: "X",
+        isClicked: "false"
+
     }
   }
   render() {
 
     return (
           <div id="ourBoard">
-              <Box index={0} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
-              <Box index={1} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
-              <Box index={2} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
-              <Box index={3} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
-              <Box index={4} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
-              <Box index={5} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
-              <Box index={6} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
-              <Box index={7} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
-              <Box index={8} isClicked={""} display={this.printOut} handleClick={this.handleClick}/>
+              <Box index={0} display={this.printOut} wasClicked = {this.handleClick}/>
+              <Box index={1} display={this.printOut} wasClicked = {this.handleClick}/>
+              <Box index={2} display={this.printOut} wasClicked = {this.handleClick}/>
+              <Box index={3} display={this.printOut} wasClicked = {this.handleClick}/>
+              <Box index={4} display={this.printOut} wasClicked = {this.handleClick}/>
+              <Box index={5} display={this.printOut} wasClicked = {this.handleClick}/>
+              <Box index={6} display={this.printOut} wasClicked = {this.handleClick}/>
+              <Box index={7} display={this.printOut} wasClicked = {this.handleClick}/>
+              <Box index={8} display={this.printOut} wasClicked = {this.handleClick}/>
           </div>
     );
   }
   printOut = () => {
 
+    console.log("X was supposed to happen");
+    return "x"
+
   }
+
   handleClick = () => {
+    console.log("Clicked");
+    return "true"
+
+
 
   }
 }
--- src/box.js
+++ src/box.js
@@ -3,13 +3,24 @@ import './App.css';
 import Board from './board.js'
 
 class Box extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {isClicked:"false"}
+  }
+
   render() {
+
     return (
-      <div id="aBox">
-        Muh box
+      <div id="aBox" onClick={this.props.wasClicked} >
+
+        {this.props.wasClicked()}
+
       </div>
     );
   }
 }
+
+
 
 export default Box;
